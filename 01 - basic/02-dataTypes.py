9849b9c65bda20d4d1b150642aebcab8f5c27270
rebornbd <60544360+rebornbd@users.noreply.github.com>
2021-09-23 11:40:54 +0600

    Update 02-dataTypes.py

--- 01 - basic/02-dataTypes.py
+++ 01 - basic/02-dataTypes.py
@@ -14,16 +14,38 @@ Dictionary
 import cmath
 
 def dataTypes():
-    num_int     = 10
-    num_float   = 20.5
-    num_complex = complex(2, 3)
+    i1 = 10
+    i2 = int(10)
+    
+    f1 = 10.0
+    f2 = float(10)
+    
+    c1 = 1 + 2j
+    c2 = complex(1, 2)
+    
+    
+    s1 = ""
+    s2 = str()
+    
+    
+    l1 = []
+    l2 = list()         # l = list([])
+    
+    
+    t1 = ()
+    t2 = tuple()
+    
+    
+    b1 = False
+    b2 = bool()         # b = bool(1)
+    
+    
+    S1 = {*()}
+    S2 = set()          # S = {1, 2, 3}
+    
+    
+    d1 = {}
+    d2 = dict()         # d = {'key1':'value1', 'key2':'value2'}
 
-    MyString = "abc"
-    myList = [1, 2, 3]
-    myTuple = (1, 2, 3)
-
-    myBool = True
-    mySet = {1, 2, 3}
-    myDict = {'key1':'value1', 'key2':'value2'}
 
 dataTypes()
